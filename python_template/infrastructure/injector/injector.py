6b2eeeb2fa81c2fc2ccb748749629873c6c5c439
cdanmontoya <carlos.danmontoya@gmail.com>
2024-05-28 19:43:17 -0500

    Black formatting

--- python_template/infrastructure/injector/injector.py
+++ python_template/infrastructure/injector/injector.py
@@ -3,6 +3,8 @@ from injector import Injector
 from python_template.infrastructure.injector.application_services_module import (
     ApplicationServicesModule,
 )
-from python_template.infrastructure.injector.repositories_module import RepositoriesModule
+from python_template.infrastructure.injector.repositories_module import (
+    RepositoriesModule,
+)
 
 injector = Injector([RepositoriesModule, ApplicationServicesModule])
