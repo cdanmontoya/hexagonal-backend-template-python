9b90a99a40e3eb0c208d64b4d16b577e9c0b6de1
cdanmontoya <carlos.danmontoya@gmail.com>
2024-07-30 01:31:10 -0500

    [#33] Rearanging folders and solving some smells

--- src/infrastructure/adapters/input/http/correlation_id/correlation_id.py
+++ src/infrastructure/adapters/input/http/correlation_id/correlation_id.py
@@ -1,23 +1,22 @@
-from fastapi import FastAPI, Request
+from typing import Awaitable, Callable
+
+from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 import uuid
 from contextvars import ContextVar
 
-# Create a context variable for the correlation ID
+from starlette.responses import Response
+
 correlation_id_ctx_var = ContextVar("correlation_id", default=None)
 
 
 class CorrelationIdMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        # Extract correlation ID from the request headers or generate a new one
+    async def dispatch(self, request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
         correlation_id = request.headers.get("X-Correlation-ID", str(uuid.uuid4()))
-        # Set the correlation ID in the context variable
         correlation_id_ctx_var.set(correlation_id)
 
-        # Pass the request to the next middleware or route handler
         response = await call_next(request)
 
-        # Add the correlation ID to the response headers
         response.headers["X-Correlation-ID"] = correlation_id
 
         return response
