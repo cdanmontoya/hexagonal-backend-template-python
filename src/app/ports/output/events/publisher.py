357b64aa900175eae484e4d1a7a4114043917cd9
cdanmontoya <carlos.danmontoya@gmail.com>
2024-08-08 19:14:44 -0500

    [#4] Trying to fix code smells

--- src/app/ports/output/events/publisher.py
+++ src/app/ports/output/events/publisher.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from collections.abc import Callable
-from typing import Any, Type
+from typing import Any
 
 from src.domain.error import Error
 from src.domain.events.event import Event
@@ -12,7 +12,7 @@ class EventPublisher(ABC):
         raise NotImplementedError
 
 
-def publishes(successful_event: Type[Event], error_event: Type[Event]) -> Any:
+def publishes(successful_event: type[Event], error_event: type[Event]) -> Any:
     """
     Convenience method to avoid transforming manually to an Event and publishing the event every single time an event
     needs to be published. This assumes that the annotated method belongs to a class that has an _event_publisher
@@ -27,7 +27,7 @@ def publishes(successful_event: Type[Event], error_event: Type[Event]) -> Any:
     """
 
     def decorator(func: Callable[..., Callable[..., Any]]) -> Callable[..., Any]:
-        def wrapper(self, *args, **kwargs) -> Any:
+        def wrapper(self: object, *args, **kwargs) -> Any:
             result = func(self, *args, **kwargs)
             event_instance = (
                 successful_event(result)
